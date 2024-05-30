b2bf87418620e1f9835576e595e7707e868643b8
Wankupi <2893353848@qq.com>
2024-05-30 18:20:02 +0800

    chore(path): update the path

--- datapath.py
+++ datapath.py
@@ -1,5 +1,5 @@
 
-FlatDataFile = '__exe__/flat.dat.csv'
+FlatDataFile = '__exe__/flat.raw.csv'
 FlatIdxFile = '__exe__/flat.idx.csv'
 
 # 每个摄像头可用的训练数据的下标
@@ -8,7 +8,7 @@ FinetuneIdxFile = '__exe__/finetune.idx.csv'
 # 预测数据,每行为一个向量,24个数字
 PredictDataFile = '__exe__/pred.fmt.csv'
 # 每个摄像头的每条预测的输入数据的下标
-PredictDataIdxFile = '__exe__/predict.idx.csv'
+PredictDataIdxFile = '__exe__/pred.map.csv'
 # 需要预测的摄像头的编号,csv,一列数字
 # PredictIdFile = '__exe__/id_for_predict.csv'
 
